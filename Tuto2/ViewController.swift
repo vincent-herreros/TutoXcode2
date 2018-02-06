322da01c275a22fad1ce2615d71a68fc1539591e
Vincent HERREROS <vincent.herreros@igtp4-mac14.isim.intra>
2018-02-06 08:44:44 +0100

    Initial Commit

---
+++ Tuto2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tuto2
+//
+//  Created by Vincent HERREROS on 06/02/2018.
+//  Copyright © 2018 Vincent HERREROS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
